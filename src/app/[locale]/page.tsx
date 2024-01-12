b8026d6d98109025f892895d5fbe0dd454995a6f
jwchoi-alticast <54067227+CJW-project@users.noreply.github.com>
2024-01-12 17:22:20 +0900

    feat : languageChanger 및 cookie set 적용

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,6 +2,7 @@ import initTranslations from '../i18n';
 import styles from './page.module.css'
 import TestClientComponent from '@/components/testClientComponent';
 import TranslationsProvider from '@/components/translationsProvider';
+import LanguageChanger from '@/components/languageChanger';
 
 export default async function Home({params: { locale }} : Record<any, any>) {
 
@@ -17,6 +18,7 @@ export default async function Home({params: { locale }} : Record<any, any>) {
       <main className={styles.main}>
         <h1>{t('title')}</h1>
         <TestClientComponent />
+        <LanguageChanger/>
       </main>
     </TranslationsProvider>
   );
